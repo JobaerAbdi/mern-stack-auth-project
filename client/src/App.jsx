65e4849610cdfccb23296cb03349f5065a0075dd
JobaerAbdi <jobaerrahman757@gmail.com>
2024-03-04 12:18:42 +0600

    react-router-dom install and routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-600">Welcome to mern auth project</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
